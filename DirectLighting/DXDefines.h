0246991231b492e830fad79553df51e3bec34585
liam <liam0neale@gmail.com>
2021-12-20 14:04:49 +0000

    linker set up

---
+++ DirectLighting/DXDefines.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <d3d12.h>
+#include "d3dx12.h"
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <dxgi1_4.h>
---
+++ DirectLighting/main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+#include "DXDefines.h"
+
+int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd)
+
+{
+
+	return 0;
+}
