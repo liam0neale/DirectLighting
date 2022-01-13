253b9126792643bed416f5ac904f7adc4d00fc6f
liam0neale <liam0neale@gmail.com>
2022-01-13 18:12:44 +0000

    broken

--- DirectLighting/VertexShader.hlsl
+++ DirectLighting/VertexShader.hlsl
@@ -1,8 +1,7 @@
 cbuffer ConstantBuffer : register(b0)
 {
-  float4 colorMultiplier;
+  float4x4 wvpMat;
 };
-
 struct VS_INPUT
 {
   float3 pos : POSITION;
@@ -17,7 +16,7 @@ struct VS_OUTPUT
 VS_OUTPUT main(VS_INPUT input)
 {
   VS_OUTPUT output;
-  output.pos = float4(input.pos, 1.0f);
-  output.color =  colorMultiplier;
+  output.pos = mul(input.pos, wvpMat);
+  output.color =  float4(1.0,0.0,0.0,1.0);//input.color;
 	return output;
 }
